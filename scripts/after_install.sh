c58620375a3750050cd4ef7c25923c8811e6a399
Rajaneesh93 <52157559+Rajaneesh93@users.noreply.github.com>
2026-03-16 19:39:21 +0530

    Update after_install.sh to handle subfolder deployment
    Added logic to move files from a subfolder after deployment and create a health check file.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
+# Move files if deployed into subfolder
+if [ -d /var/www/html/my-production-app ]; then
+  cp -r /var/www/html/my-production-app/* /var/www/html/
+  rm -rf /var/www/html/my-production-app
+fi
+echo "OK" > /var/www/html/health
 chown -R apache:apache /var/www/html
 chmod -R 755 /var/www/html
